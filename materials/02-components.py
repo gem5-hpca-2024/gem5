f7353b86ab6acec457b8844bdcdf3f8b86e2e7a3
Bobby R. Bruce <bbruce@ucdavis.edu>
2024-03-02 22:19:45 +0000

    misc: Fix import bugs in 02 materials and KVM import in 03
    Change-Id: I2629a45d806c7a91a3d2967269684c6c1027c7c9

--- materials/02-components.py
+++ materials/02-components.py
@@ -1,5 +1,7 @@
-from gem5.coherence_protocol import CoherenceProtocol
 from gem5.components.boards.x86_board import X86Board
+from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
+    MESITwoLevelCacheHierarchy,
+)
 from gem5.components.memory.single_channel import SingleChannelDDR3_1600
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.components.processors.simple_switchable_processor import (
@@ -9,4 +11,3 @@ from gem5.isas import ISA
 from gem5.resources.resource import obtain_resource
 from gem5.simulate.exit_event import ExitEvent
 from gem5.simulate.simulator import Simulator
-from gem5.utils.requires import requires
